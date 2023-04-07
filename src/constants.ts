ae7a53b5a82e1c27bc1b22769d5bdc2e6e14803b
Pablo del Barrio <pablodbb@gmail.com>
2023-04-07 20:41:49 +0200

    first commit app is almost completed

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+export const SUPPORTED_LANGUAGES = {
+  es: 'Español',
+  en: 'English',
+  de: 'Deutch',
+}
+
+export const VOICE_FOR_LANGUAGE = {
+  en: 'en-GB',
+  es: 'es-MX',
+  de: 'de-DE',
+}
+
+export const AUTO_LANGUAGE = 'auto'
