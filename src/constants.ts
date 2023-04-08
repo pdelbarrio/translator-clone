1a8e904261b7e7a6e6d75a1ab86694b255a7d405
Pablo del Barrio <pablodbb@gmail.com>
2023-04-08 17:38:07 +0200

    added more languages

--- src/constants.ts
+++ src/constants.ts
@@ -2,12 +2,24 @@ export const SUPPORTED_LANGUAGES = {
   es: 'Español',
   en: 'English',
   de: 'Deutch',
+  fr: 'Français',
+  it: 'Italiano',
+  pt: 'Português',
+  no: 'Norsk',
+  ca: 'Català',
+  pl: 'Polski',
 }
 
 export const VOICE_FOR_LANGUAGE = {
   en: 'en-GB',
   es: 'es-MX',
   de: 'de-DE',
+  fr: 'fr-FR',
+  it: 'it-IT',
+  pt: 'pt-PT',
+  no: 'no-NO',
+  ca: 'ca-ES',
+  pl: 'pl-PL',
 }
 
 export const AUTO_LANGUAGE = 'auto'
